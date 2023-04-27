1ec4305927c11f6230ecb52e38875241fe0c2c16
JongHyunParkDev <eomgr88@gmail.com>
2023-04-27 09:24:14 +0900

    1차 base

---
+++ JdaBot/src/main/java/com/yj/yjbot/lavaplayer/PlayerManager.java
@@ -0,0 +1,28 @@
+package com.yj.yjbot.lavaplayer;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PlayerManager {
+
+    private static PlayerManager INSTANCE;
+
+    private Map<Long, GuildMusicManager> quildMusicManagers = new HashMap<>();
+    private AudioPlayerManager audioPlayerManager = new DefaultAudioPlayerManager();
+
+    private PlayerManager() {
+        AudioSourceManagers.registerRemoteSources(audioPlayerManager);
+        AudioSourceManagers.registerLocalSource(audioPlayerManager);
+    }
+
+    public static PlayerManager get() {
+        if (INSTANCE == null) {
+            INSTANCE = new PlayerManager();
+        }
+        return INSTANCE;
+    }
+}
